9b6ee1efb8c42ae985fb69b715e41d5e63ea6aa6
Darren <darrenxychen@gmail.com>
2023-10-16 16:46:56 -0700

    updated website paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Login from './pages/Login'
 import Home from './pages/home-page'
+import Register from './pages/Register'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
 
     </div>
