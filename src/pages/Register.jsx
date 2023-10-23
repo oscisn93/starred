d964e42c35009233c2eba295908ee2130fd96127
darren <darrenxychen@gmail.com>
2023-10-22 19:05:44 -0700

    added login link to reg page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,23 +4,27 @@ import { Link } from 'react-router-dom'
 
 function Register() {
 
-    return (
+  return (
+    <div>
       <div>
-        <div>
-            <Link to="/home">Go to Home Page</Link>
-        </div>
-        <div className='header'>
-            <h1>Starred</h1>
-        </div>
-        <div className='login-form'>
-            <h2>Register</h2>
-            <input type="email" placeholder='Email' />
-            <input type="password" placeholder='Password' />
-            <input type="password" placeholder='Confirm Password' />
-            <button>Submit</button>
+        <Link to="/home">Go to Home Page</Link>
+      </div>
+      <div className='header'>
+        <h1>Starred</h1>
+      </div>
+      <div className='login-form'>
+        <h2>Register</h2>
+        <input type="email" placeholder='Email' />
+        <input type="password" placeholder='Password' />
+        <input type="password" placeholder='Confirm Password' />
+        <button>Submit</button>
+        <div className="register-btn">
+          <p>Already have an account? <a href="/">Log in</a></p>
+
         </div>
       </div>
-    )
-  }
-  
-  export default Register;
+    </div>
+  )
+}
+
+export default Register;
