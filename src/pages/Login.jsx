67d1cfe4d4bbbce89b168fb754936ab6b43c862a
darren <darrenxychen@gmail.com>
2023-10-24 23:39:34 -0700

    added loading btn, updated form requirements, css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,13 @@
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
-import React from "react";
+import React, { useState } from "react";
 import { signIn } from "../services/firebase";
 
 function Login() {
   const navigate = useNavigate();
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   function handleEmail(event) {
     setEmail(event.target.value);
@@ -18,27 +19,42 @@ function Login() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    setLoading(true);
     signIn(email, password).then((user) => {
       console.log(user);
+      setLoading(false);
       return navigate("/home");
     });
   }
 
   return (
-    <div>
+    <div className="login">
       <div className="header">
         <h1>Starred</h1>
       </div>
       <div className="login-form">
         <h2>Login</h2>
-        <input type="email" placeholder="Email" onChange={handleEmail} />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={handleEmail}
+          required
+        />
         <input
           type="password"
           placeholder="Password"
           onChange={handlePassword}
+          required
         />
+        {/* <div className="remember-me">
+          <input type="checkbox" />
+          <p>Remember me</p>
+        </div> */}
+
         <div className="submit-btn">
-          <button onClick={handleSubmit}>Login</button>
+          <button onClick={handleSubmit}>
+            {loading ? "Logging in..." : "Login"}
+          </button>
         </div>
         <div className="register-btn">
           <p>Don't have an account? <a href="/register">Sign Up</a></p>
